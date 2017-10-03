e8102402bfdd42e3c639783a3cce390c41698c3f
pacochak <katarzynapacocha1@gmail.com>
2017-10-03 22:42:01 +0200

    Get plays from API.

--- src/Containers/PlaysPage/PlaysPage.js
+++ src/Containers/PlaysPage/PlaysPage.js
@@ -1,17 +1,67 @@
-import React from 'react';
-import { Link } from 'react-router-dom'
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import fetch from 'isomorphic-fetch';
 
+export default class PlaysPage extends Component {
+	constructor(props) {
+	  super(props);
+	  this.state = {
+	    plays: []
+	  };
+	}
 
-const PlaysPage = ({ match }) => (
-  <div>
-    Browse Plays
-    <ul>
-      <li><Link to={`${match.path}/1`}>1</Link></li>
-      <li><Link to={`${match.path}/2`}>2</Link></li>
-      <li><Link to={`${match.path}/3`}>3</Link></li>
-      <li><Link to={`${match.path}/4`}>4</Link></li>
-    </ul>
-  </div>
-)
+	componentWillMount(){
+    fetch('http://localhost:3001/plays')
+      .then((response) => {
+        return response.json()
+      })
+      .then((plays) => {
+        this.setState({ plays })
+      })
+  }
 
-export default PlaysPage;
+	__renderplay(play) {
+		return(
+			<tr key={play.playId}>
+				<td>
+					<Link to={`${this.props.match.path}/${play.playId}`}>{play.playId}</Link>
+				</td>
+				<td>
+					{play.date}
+				</td>
+				<td>
+					{play.gameId}
+				</td>
+			</tr>
+			
+		);
+	}
+
+  __renderTable() {
+
+  	return(
+	    <div>
+		    <h3>Browse plays</h3>
+
+		    <table>
+		    	<tbody>
+		    		<tr>
+		    			<th></th>
+		    			<th>Date</th>
+		    			<th>Game id</th>
+		    		</tr>
+		    	{this.state.plays && this.state.plays.map(play => this.__renderplay(play))}
+		    	</tbody>
+		    </table>
+		  </div>
+  	);
+  }
+
+  render() {
+    return (
+    	<div className="playsPage">
+		    {this.__renderTable()}
+		  </div>
+    );
+  }
+}
