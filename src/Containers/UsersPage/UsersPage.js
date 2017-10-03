287b7c0724bdb28de5eecf20094004bdae48a05a
pacochak <katarzynapacocha1@gmail.com>
2017-10-03 22:36:32 +0200

    Get users from API.

--- src/Containers/UsersPage/UsersPage.js
+++ src/Containers/UsersPage/UsersPage.js
@@ -1,17 +1,71 @@
-import React from 'react';
-import { Link } from 'react-router-dom'
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import fetch from 'isomorphic-fetch';
 
+export default class UsersPage extends Component {
+	constructor(props) {
+	  super(props);
+	  this.state = {
+	    users: []
+	  };
+	}
 
-const UsersPage = ({ match }) => (
-  <div>
-    Browse Users
-    <ul>
-      <li><Link to={`${match.path}/1`}>Brad</Link></li>
-      <li><Link to={`${match.path}/2`}>Chris</Link></li>
-      <li><Link to={`${match.path}/3`}>Michael</Link></li>
-      <li><Link to={`${match.path}/4`}>Ryan</Link></li>
-    </ul>
-  </div>
-)
+	componentWillMount(){
+    fetch('http://localhost:3001/users')
+      .then((response) => {
+        return response.json()
+      })
+      .then((users) => {
+        this.setState({ users })
+      })
+  }
 
-export default UsersPage;
+	__renderUser(user) {
+		return(
+			<tr key={user.userId}>
+				<td>
+					<Link to={`${this.props.match.path}/${user.userId}`}>{user.userId}</Link>
+				</td>
+				<td>
+					{user.login}
+				</td>
+				<td>
+					{user.userName}
+				</td>
+				<td>
+					{user.email}
+				</td>
+			</tr>
+			
+		);
+	}
+
+  __renderTable() {
+
+  	return(
+	    <div>
+		    <h3>Browse Users</h3>
+
+		    <table>
+		    	<tbody>
+		    		<tr>
+		    			<th></th>
+		    			<th>Login</th>
+		    			<th>User name</th>
+		    			<th>Date of add</th>
+		    		</tr>
+		    	{this.state.users && this.state.users.map(user => this.__renderUser(user))}
+		    	</tbody>
+		    </table>
+		  </div>
+  	);
+  }
+
+  render() {
+    return (
+    	<div className="UsersPage">
+		    {this.__renderTable()}
+		  </div>
+    );
+  }
+}
