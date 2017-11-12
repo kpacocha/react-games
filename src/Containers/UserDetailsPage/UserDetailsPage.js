91a7e754e8b305c6f828d24214b34a6fcd8e8e94
pacochak <katarzynapacocha1@gmail.com>
2017-11-12 21:17:30 +0100

    User details page - use cards from material ui.

--- src/Containers/UserDetailsPage/UserDetailsPage.js
+++ src/Containers/UserDetailsPage/UserDetailsPage.js
@@ -1,14 +1,41 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom'
+import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+import FlatButton from 'material-ui/FlatButton';
+import FontIcon from 'material-ui/FontIcon';
+	// <div>
+	// 	<h5>User #{match.params.userId} details</h5>
+	//   <nav className="context-nav">
+	//     <NavLink to={`${match.params.userId}/edit`} exact activeClassName="active">Edit user</NavLink>
+	//   </nav>
+ //  </div>
 
+    //  <CardMedia
+    //   overlay={<CardTitle title="Overlay title" subtitle="Overlay subtitle" />}
+    // >
+    //   <img src="images/nature-600-337.jpg" alt="" />
+    // </CardMedia>
 
 const UserDetailsPage = ({ match }) => (
-	<div>
-		<h5>User #{match.params.userId} details</h5>
-	  <nav className="context-nav">
-	    <NavLink to={`${match.params.userId}/edit`} exact activeClassName="active">Edit user</NavLink>
-	  </nav>
-  </div>
+
+  <Card>
+    <CardHeader
+      title={`User #${match.params.userId} details`}
+      subtitle=""
+      avatar="images/jsa-128.jpg"
+    />
+    <CardTitle 
+    	title={`User #${match.params.userId} details`}
+    	subtitle="" 
+    />
+    <CardText>
+    </CardText>
+    <CardActions>
+      <FlatButton label="Delete" />
+      <FlatButton label="Edit user data" primary href={`${match.params.userId}/edit`}/>
+    />
+    </CardActions>
+  </Card>
 )
 
 export default UserDetailsPage;
