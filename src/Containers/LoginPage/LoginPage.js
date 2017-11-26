ddb240f51c668e497d0ec301e73c6fbf4a7e4fe0
pacochak <katarzynapacocha1@gmail.com>
2017-11-26 21:34:10 +0100

    Login page.

--- src/Containers/LoginPage/LoginPage.js
+++ src/Containers/LoginPage/LoginPage.js
@@ -1,19 +1,25 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { login } from '../../utils/xhr'
+import RaisedButton from 'material-ui/RaisedButton';
+import Paper from 'material-ui/Paper';
 
 const LoginPage = ({ history }) => (
   <div>
+    <Paper>
     <h1>Login Page</h1>
     <p>
       For this example application, we cannot visit <Link to="/app">/app</Link> until we are logged in.
       Clicking the "Login" button will simulate a login by setting Redux state.
     </p>
-    <button onClick={() => {
+
+    <RaisedButton label="Login" primary={true} onClick={() => {
       login().then(() => {
         history.push('/app')
       })
-    }}>Login</button>
+    }} />
+
+    </Paper>
   </div>
 )
 
