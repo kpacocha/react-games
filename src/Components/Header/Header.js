aa8be793f9f58073fdba1ff58bcbab12a526aea5
pacochak <katarzynapacocha1@gmail.com>
2017-12-10 22:28:43 +0100

    User menu.

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
 import IconButton from 'material-ui/IconButton';
+import PersonIcon from 'material-ui/svg-icons/social/person';
 import MenuItem from 'material-ui/MenuItem';
 import RaisedButton from 'material-ui/RaisedButton';
+import FlatButton from 'material-ui/FlatButton';
 import {Toolbar, ToolbarGroup, ToolbarSeparator, ToolbarTitle} from 'material-ui/Toolbar';
+import IconMenu from 'material-ui/IconMenu';
+import Avatar from 'material-ui/Avatar';
+import ListItem from 'material-ui/List/ListItem';
+import { logout } from '../../utils/xhr'
 
 import './Header.css';
 
 export default class Header extends Component {
+
+	handleClick() {
+	  alert('You clicked the Chip.');
+	}
+
   __renderHeader() {
+  	const userName = 'Kate';
   	return (
   		<Toolbar>
   			<ToolbarTitle text="Save Your Result" />
@@ -17,6 +29,17 @@ export default class Header extends Component {
         	<MenuItem primaryText="Games" href="/app/games" />
         	<MenuItem primaryText="Plays" href="/app/plays" />
           <ToolbarSeparator />
+
+		      <IconMenu
+			      iconButtonElement={<IconButton><PersonIcon /></IconButton>}
+			      anchorOrigin={{horizontal: 'left', vertical: 'top'}}
+			      targetOrigin={{horizontal: 'left', vertical: 'top'}}
+			    >
+			    	<ListItem leftAvatar={<Avatar>{userName[0]}</Avatar>}> {userName} </ListItem>
+			      <MenuItem primaryText="Details" />
+			      <MenuItem primaryText="Log out" />
+			    </IconMenu>
+
           <RaisedButton label="Add new play" primary={true} href="/app/plays/add" />
         </ToolbarGroup>
       </Toolbar>
